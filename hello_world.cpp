27168df00582f3fe18a187242d499aa5a0e9d675
Andrey52525252 <andrewkenunen@gmail.com>
2026-02-12 15:15:26 +0300

    added different mistakes for walgrind

--- hello_world.cpp
+++ hello_world.cpp
@@ -8,6 +8,16 @@ int main() {
     std::cout << "hello, world" << std::endl;
 
     int* a = new int(52);
-    
+
+    int* array = new int[10];
+    std::cout << array[11] << std::endl;
+
+    int x;
+    std::cout << x << std::endl;
+
+    int* p1 = new int;
+    free(p1);
+
+
     return 0;
 }
